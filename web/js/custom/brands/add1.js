1d6bce803c2bbbe12fa615ca247478d1244b3f46
chengxiaowan <45581705+chengxiaowan@users.noreply.github.com>
2019-06-25 08:30:18 +0800

    备份一次

--- web/js/custom/brands/add1.js
+++ web/js/custom/brands/add1.js
@@ -11,7 +11,10 @@ var config = {
     api_user: api_url + '/user/userList3', //获取负责人列表
     api_token: api_url + '/qiniu/getUpToken', //获取七牛的token
     api_userlist: api_url + '/shopsBrand/shopsBrandShopsOwnerList', //获取门店品牌负责人
-    api_delUser: api_url + '/user/changeIsQuit' //离职门店品牌负责人
+    api_delUser: api_url + '/user/changeIsQuit', //离职门店品牌负责人
+    api_getgoods: api_url + "/shopsBrand/shopsBrandGoodsList", //获取品牌商品列表
+    api_delgoods: api_url + "/shopsBrand/delShopsBrandGoods", //获取品牌商品列表
+    api_gettype: api_url + "/type/dataList", //获取品牌商品列表
 
 
 
@@ -37,9 +40,16 @@ window.app = new Vue({
         imagePath: '', //封面地址
         shopsBrandPicList: [], //店铺图片列表
 
-        tokenMessage: "",
-        shopsBrandId: '',
-        keywords: ""
+        tokenMessage: "", //七牛云的token
+        shopsBrandId: '', //品牌ID
+        keywords: "", //搜索关键字
+
+        //商品筛选
+        type: "", //分类列表
+        typeId: "", //用户选择的分类
+        minPrice: "", //用户输入的最低价
+        maxPrice: "", //用户输入的最高价
+        check: "",
 
 
 
@@ -76,7 +86,7 @@ window.app = new Vue({
             else this.loadingSwitch = layer.load(3);
         },
 
-        // 切换tab
+        // 切换tab时执行的方法
         activateTab: function (index_chosen) {
             const that = this;
             that.index = index_chosen;
@@ -86,10 +96,11 @@ window.app = new Vue({
             } else if (this.index == 2) {
                 // that.getColorList();
                 that.getbranduser(); //获取门店品牌负责人信息
-                console.log("2")
+                console.log("获取门店负责人")
             } else if (this.index == 3) {
-                // that.getModelList();
-                console.log("3")
+                that.getgoods();
+                that.getType()
+                console.log("获取品牌商品")
 
             } else if (this.index == 4) {
                 // that.getSkuList();
@@ -129,7 +140,7 @@ window.app = new Vue({
                         console.log(res)
                         that.bandname = res.result.brandName //品牌名称
                         that.userId = res.result.userId //负责人id
-                        that.labels = res.result.labels //标签
+                        $("#tagsinputval").attr("value",res.result.labels.toString())
                         that.summary = res.result.summary
                         that.description = res.result.description; //图文描述
                         that.logoPath = res.result.logoPath
@@ -190,6 +201,60 @@ window.app = new Vue({
         },
 
 
+        //获取品牌商品列表
+        getgoods(page, keywords) {
+            var that = this
+            $('body,html').scrollTop(0)
+            if (page) this.list.pageNum = page
+            var that = this;
+            that.loading();
+            $.ajax({
+                url: config.api_getgoods,
+                async: true,
+                type: 'post',
+                data: {
+                    shopsBrandId: shopsBrandId,
+                    keywords: that.keywords,
+                    typeId: that.typeId,
+                    maxPrice: that.maxPrice,
+                    minPrice: that.minPrice,
+                    auditStatus: that.check,
+                    pageSize: that.list.pageSize || 10,
+                    pageNo: that.list.pageNum || 1,
+                },
+                success: function (res) {
+                    console.log(res)
+                    that.loading('close')
+                    // console.log(res)
+                    if (res.error == "00") {
+                        that.list = res.result;
+                        console.log(that.list)
+                        //分页
+                        if (that.pagi) {
+                            $('.pagi').pagination('updatePages', that.list.pages)
+                            if (page == 1) $('.pagi').pagination('goToPage', that.list.pageNum)
+                        } else {
+                            that.pagi = $('.pagi').pagination({
+                                pages: that.list.pages, //总页数
+                                showCtrl: true,
+                                displayPage: 6,
+                                currentPage: that.list.pageNum,
+                                onSelect: function (num) {
+                                    that.list.pageNum = num
+                                    that.getgoods()
+                                    yo.scrollTo('body')
+                                }
+                            })
+                        }
+                    } else {
+                        layer.msg(res.msg)
+                    }
+                }
+            });
+        },
+
+
+
         // 新建负责人
         jumpToHeader() {
             var index = layer
@@ -225,6 +290,13 @@ window.app = new Vue({
             that.getbranduser(page, keywords);
         },
 
+        search2() {
+            const that = this;
+            var page = 1;
+            var keywords = that.keywords;
+            that.getgoods(page, keywords);
+        },
+
         //保存数据  需要修改
         save() {
             //整理数据
@@ -238,22 +310,51 @@ window.app = new Vue({
             that.clearDate($("#info6"), arr)
             // console.log(shopsBrandPicList)
 
-            console.log(document.getElementById("boboname").value)
-            console.log(that.userId)
-            console.log(document.getElementById("tagsinputval").value)
-            console.log(that.summary)
-            console.log(that.editor.getContent())
-            console.log($('#vivew').attr('src'))
-            console.log($('#vivew1').attr('src'))
-            console.log(arr)
+            // console.log(document.getElementById("boboname").value)
+            // console.log(that.userId)
+            // console.log(document.getElementById("tagsinputval").value)
+            // console.log(that.summary)
+            // console.log(that.editor.getContent())
+            // console.log($('#vivew').attr('src'))
+            // console.log($('#vivew1').attr('src'))
+            // console.log(arr)
 
+            //提交前先验证
 
+            //提交到服务器
 
-            if (arr.length != 0) {
-                //提交到服务器
-                const that = this;
+            if ($("#boboname").val() == "") {
+                layer.msg("请输入品牌名称", {
+                    time: 3000
+                })
+            } else if (that.userId == "") {
+                layer.msg("请选择品牌负责人", {
+                    time: 3000
+                })
+            } else if (document.getElementById("tagsinputval").value == "") {
+                layer.msg("请输入品牌标签", {
+                    time: 3000
+                })
+            } else if (that.summary == "") {
+                layer.msg("请输入品牌简介", {
+                    time: 3000
+                })
+            } else if (that.editor.getContent() == "") {
+                layer.msg("请添加品牌图文详情", {
+                    time: 3000
+                })
+            } else if ($('#vivew').attr('src') == "../images/imgadd.png") {
+                layer.msg("请选择品牌lLOGO", {
+                    time: 3000
+                })
+            } else if ($('#vivew1').attr('src') == "../images/imgadd.png") {
+                layer.msg("请选择店铺封面图", {
+                    time: 3000
+                })
+            } else {
+
                 $.ajax({
-                    url: config.api_save,
+                    url: config.api_edit,
                     traditional: true,
                     async: true,
                     type: 'post',
@@ -265,7 +366,7 @@ window.app = new Vue({
                         description: that.editor.getContent(), //富文本
                         logoPath: $('#vivew').attr('src'), //logo地址
                         imagePath: $('#vivew1').attr('src'), //封面
-                        shopsBrandPicList: JSON.stringify(arr) //图片列表
+                        shopsBrandPicList: ""
                     },
                     success: res => {
                         console.log(res);
@@ -275,6 +376,7 @@ window.app = new Vue({
                     }
                 })
             }
+
 
 
         },
@@ -340,9 +442,11 @@ window.app = new Vue({
         },
         //dy测试Vue双向绑定是否正常
         dy() {
-            console.log("labels")
-            console.log(this.labels)
-            console.log(document.getElementById("tagsinputval").value, )
+            var that = this
+            console.log(that.maxPrice)
+            console.log(that.minPrice)
+            console.log(that.check)
+            console.log(that.typeId)
         },
 
         //删除图片
@@ -352,17 +456,22 @@ window.app = new Vue({
 
         //门店品牌负责人离职
         delUser(id) {
-            $.ajax({
-                url: config.api_delUser,
-                data: {
-                    userId: id
-                },
-                success: res => {
-                    layer.msg(res.msg)
-                    this.getbranduser();
-
-                }
+            const that = this;
+            const dialog = layer.confirm("确认该负责人已经离职？", {
+                title: "提示"
+            }, () => {
+                $.ajax({
+                    url: config.api_delUser,
+                    data: {
+                        userId: id
+                    },
+                    success: res => {
+                        layer.msg(res.msg);
+                        that.getbranduser()
+                    }
+                })
             })
+
         },
 
         //查看门店品牌负责人详情
@@ -378,9 +487,88 @@ window.app = new Vue({
                         that.getbranduser();
                     }
                 });
+        },
+
+        //商品添加弹窗
+        addgoods() {
+            var that = this
+            var index = layer
+                .open({
+                    type: 2,
+                    title: '添加商品',
+                    content: 'addgoods.html?id=' + shopsBrandId,
+                    area: ['100%', '100%'],
+                    end: function () {
+                        that.getgoods()
+                    }
+                });
+        },
+
+        //删除品牌商品---layer弹窗确认
+        delgoods(id) {
+            var that = this
+            var queding = layer.confirm("您确认删除该商品么？", {
+                title: "删除商品"
+            }, () => {
+                $.ajax({
+                    url: config.api_delgoods,
+                    type: "post",
+                    async: true,
+                    data: {
+                        shopsBrandGoodsId: id
+                    },
+                    success: res => {
+                        console.log(res)
+                        if (res.error == "00") {
+                            layer.msg("删除成功", {
+                                time: 3000
+                            })
+                            that.getgoods()
+                        }
+                    }
+                })
+            })
+        },
+
+        //获取一级分类信息
+        getType() {
+            var that = this;
+            $.ajax({
+                url: config.api_gettype,
+                type: "post",
+                async: true,
+                data: {
+                    level: "1"
+                },
+                success: res => {
+                    console.log(res)
+                    if (res.error == "00") {
+                        that.type = res.result
+
+                    } else {
+                        layer.msg("分类获取错误" + res.error)
+                    }
+                }
+            })
         }
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //简介文字计数
---
+++ web/js/custom/brands/addgoods.js
@@ -0,0 +1,125 @@
+var shopsBrandId = parameter().id;
+
+var config = {
+    role: localStorage.userRole,
+    api_getgoods: api_url + "/goods/dataList3",
+    api_addgoods: api_url + '/shopsBrand/batchAddShopsBrandGoodsList' //新增品牌商品 也可用于批量
+}
+
+//初始化Vue实例
+
+window.app = new Vue({
+    el: "#app",
+    data: {
+        list: {},
+        keywords: "",
+    },
+    methods: {
+
+        /**
+         * 载入中方法
+         *
+         * @param {string} s 是否关闭
+         */
+        loading: function (s) {
+            if (s == "close") layer.close(this.loadingSwitch)
+            else this.loadingSwitch = layer.load(3);
+        },
+
+
+        //获取全部商品列表
+        getgoods(page, keywords) {
+            var that = this
+            $('body,html').scrollTop(0)
+            if (page) this.list.pageNum = page
+            var that = this;
+            that.loading();
+
+            $.ajax({
+                url: config.api_getgoods,
+                type: "post",
+                async: true,
+                data: {
+                    keywords: that.keywords,
+                    pageSize: that.list.pageSize || 10,
+                    pageNo: that.list.pageNum || 1,
+                },
+                success: function (res) {
+                    that.loading('close')
+                    // console.log(res)
+                    that.list = res.result
+                    console.log(that.list)
+                    // console.log(that.list.total)
+                    if (res.error == "00") {
+                        that.list = res.result;
+                        //分页
+                        if (that.pagi) {
+                            $('.pagi').pagination('updatePages', that.list.pages)
+                            if (page == 1) $('.pagi').pagination('goToPage', that.list.pageNum)
+                        } else {
+                            that.pagi = $('.pagi').pagination({
+                                pages: that.list.pages, //总页数
+                                showCtrl: true,
+                                displayPage: 6,
+                                currentPage: that.list.pageNum,
+                                onSelect: function (num) {
+                                    that.list.pageNum = num
+                                    that.getgoods()
+                                    yo.scrollTo('body')
+                                }
+                            })
+                        }
+                    } else {
+                        layer.msg(res.msg)
+                    }
+                }
+            })
+        },
+
+        //测试Vue是否绑定的代码
+        dy() {
+            console.log(this.list)
+        },
+
+        //添加品牌商品
+        addgoods(id) {
+            var that = this;
+            $.ajax({
+                url: config.api_addgoods,
+                type: "post",
+                async: true,
+                data: {
+                    shopsBrandId: shopsBrandId, //品牌ID
+                    goodsList: id //商品ID
+
+                },
+                success: res => {
+                    // console.log(res)
+                    if (res.error == "00") {
+                        layer.msg("添加成功", {
+                            time: 3000
+                        })
+                    } else {
+                        layer.msg(res.mag, {
+                            time: 3000
+                        })
+                    }
+                }
+            })
+        },
+
+        //搜索
+        search() {
+            const that = this;
+            var page = 1;
+            var keywords = that.keywords;
+            that.getgoods(page, keywords);
+        },
+    },
+
+    //钩子函数
+    mounted: function () {
+        var that = this
+        that.getgoods();
+    },
+})
